8cc334228f332d054a7769398d74f4a0a3a1b43a
brucerry <bruce.cheung111@gmail.com>
2022-04-21 00:18:58 +0800

    EP.19 Maths in OpenGL

--- src/Shader.cpp
+++ src/Shader.cpp
@@ -106,20 +106,25 @@ void Shader::Unbind() const
 
 void Shader::SetUniform1i(const std::string& name, int value)
 {
-	GLCall(glUniform1i(GetUniform4fLocation(name), value));
+	GLCall(glUniform1i(GetUniformLocation(name), value));
 }
 
 void Shader::SetUniform1f(const std::string& name, float value)
 {
-	GLCall(glUniform1f(GetUniform4fLocation(name), value));
+	GLCall(glUniform1f(GetUniformLocation(name), value));
 }
 
 void Shader::SetUniform4f(const std::string& name, float v0, float v1, float v2, float v3)
 {
-	GLCall(glUniform4f(GetUniform4fLocation(name), v0, v1, v2, v3));
+	GLCall(glUniform4f(GetUniformLocation(name), v0, v1, v2, v3));
 }
 
-int Shader::GetUniform4fLocation(const std::string& name)
+void Shader::SetUniformMat4f(const std::string& name, const glm::mat4& matrix)
+{
+	glUniformMatrix4fv(GetUniformLocation(name), 1, GL_FALSE, &matrix[0][0]);
+}
+
+int Shader::GetUniformLocation(const std::string& name)
 {
 	if (m_UniformLocationCache.contains(name))
 	{
--- src/Shader.h
+++ src/Shader.h
@@ -3,6 +3,8 @@
 #include <string>
 #include <unordered_map>
 
+#include "glm/glm.hpp"
+
 struct ShaderProgramSource
 {
 	std::string VertexSource;
@@ -22,12 +24,13 @@ public:
 	void SetUniform1i(const std::string& name, int value);
 	void SetUniform1f(const std::string& name, float value);
 	void SetUniform4f(const std::string& name, float v0, float v1, float v2, float v3);
+	void SetUniformMat4f(const std::string& name, const glm::mat4& matrix);
 private:
-	int GetUniform4fLocation(const std::string& name);
-
-	unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader);
-	unsigned int CompileShader(unsigned int type, const std::string& source);
 	ShaderProgramSource ParseShader(const std::string& filepath);
+	unsigned int CompileShader(unsigned int type, const std::string& source);
+	unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader);
+
+	int GetUniformLocation(const std::string& name);
 
 private:
 	unsigned int m_RendererID;
